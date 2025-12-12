3dc9fc25b7400b7a00a669dc5537ac127c014b65
Sheriff .n Ibrahim <ibrahimsheriff999@gmail.com>
2025-12-12 10:43:49 +0100

    CVE security patch fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "@/components/layout-wrapper";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Jenrianna Apartments",
@@ -25,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto`}>
+      <body className={`antialiased mx-auto`}>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
